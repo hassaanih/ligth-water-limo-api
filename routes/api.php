6afb64db7eb3d2b4a75fb035531f47ca1e712ad6
Hassaan <hassaanih1997@gmail.com>
2023-04-18 23:25:16 +0500

    minor fix

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('abc/abc', 'App\Http\Controllers\TestController@create');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
