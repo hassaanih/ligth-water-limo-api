ddcbb2e35a063a0c900ead133a0be690269ca30b
unknown <hassaanih1997@gmail.com>
2023-05-14 01:23:14 +0500

    minor fix

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         App\Providers\SmsServiceProvider::class,
+        Stripe\StripeServiceProvider::class,
         /*
          * Package Service Providers...
          */
